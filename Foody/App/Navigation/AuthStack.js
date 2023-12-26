c31e0fb5f45cb684e228f0e534f0c58a423ad486
Shivam Jha <jhashivam1804@gmail.com>
2023-12-26 19:27:59 +0530

    Commit Day 1 Take 2

--- Foody/App/Navigation/AuthStack.js
+++ Foody/App/Navigation/AuthStack.js
@@ -2,6 +2,7 @@
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import React, { Component } from 'react';
 import SignIn from '../Screens/Auth/SignIn';
+import Slider from '../Screens/Auth/Slider';
 
 const Stack = createStackNavigator();
 
@@ -13,7 +14,8 @@ const AuthStack = () => {
                 headerShown: false,
             }}
         >
-            <Stack.Screen name="SignIn" component={SignIn} />
+            {/* <Stack.Screen name="SignIn" component={SignIn} /> */}
+            <Stack.Screen name="Slider" component={Slider}/>
         </Stack.Navigator>
     );
 };
